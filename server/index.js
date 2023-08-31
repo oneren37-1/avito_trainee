be7552f83fd9b94676b8191f30bfad73848fe675
oneren37-1 <oneren38@gmail.com>
2023-08-31 19:56:09 +0300

    server cache

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const app = express()
 const cors = require('cors');
+const NodeCache = require('node-cache');
 const GameDTO = require('./dto/Game');
 const GamesDTO = require('./dto/Games');
+
+const app = express()
+const cache = new NodeCache();
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +13,11 @@ app.use(express.json());
 const BASE_URL = 'https://www.freetogame.com/api';
 app.get('/games', (req, res) => {
     const { page, size, platform, sort, category } = req.query;
+
+    const cachedData = cache.get('games_' + JSON.stringify({ platform, sort, category }));
+    if (cachedData) {
+        return res.status(200).send(new GamesDTO(cachedData, page, size));
+    }
 
     const subUrl = category ? 'filter' : 'games';
     const url = new URL(`${BASE_URL}/${subUrl}`);
@@ -25,6 +33,7 @@ app.get('/games', (req, res) => {
                 return res.status(404).send('Not found');
             }
             res.status(200).send(new GamesDTO(data, page, size))
+            cache.set('games_' + JSON.stringify({ platform, sort, category }), data, 60 * 60 * 24);
         })
         .catch((err) => {
             console.log(err);
@@ -35,6 +44,11 @@ app.get('/games', (req, res) => {
 app.get('/game', (req, res) => {
     if (!req.query.id) return res.status(400).send('Bad request');
 
+    const cachedData = cache.get('game_' + req.query.id);
+    if (cachedData) {
+        return res.status(200).send(new GameDTO(cachedData));
+    }
+
     fetch(`${BASE_URL}/game?id=${req.query.id}`)
         .then(data => data.json())
         .then(data => {
@@ -42,6 +56,7 @@ app.get('/game', (req, res) => {
                 return res.status(404).send('Not found');
             }
             res.status(200).send(new GameDTO(data));
+            cache.set('game_' + req.query.id, data, 60 * 60 * 24);
         })
         .catch((err) => {
             console.log(err);
